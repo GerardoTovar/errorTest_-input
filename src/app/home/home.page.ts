8e554d4cf77ee6d76c8c52ff339c157453425d00
Gerardo Emmanuel Tovar Barragan <gerardo.tovar@lestprototype.com>
2024-06-09 00:59:05 -0700

    lesscode

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,30 +11,12 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class HomePage {
   public fb = inject(FormBuilder);
+  public EmailError = 'un error'
+  public EmailError2 = ''
   public registerForm: FormGroup = this.fb.group({
     email: ['',[Validators.required, Validators.email, Validators.maxLength(100)]]
   });
 
-  get EmailError() {
-    return this.msgError('email')
-  }
 
-  public ErrorMsg:any = {
-    email: { 
-      required: "Email es requerido", 
-      email: "Email debe ser valido",
-      maxlength: 'Email es largo'
-    }
-  }
 
-  msgError(campo:string) :string | void {
-    if(this.registerForm.get(campo)?.errors?.['required'])  return this.ErrorMsg[campo]['required']
-    if(this.registerForm.get(campo)?.errors?.['minlength']) return this.ErrorMsg[campo]['minlength']
-    if(this.registerForm.get(campo)?.errors?.['maxlength']) return this.ErrorMsg[campo]['maxlength']
-    if(this.registerForm.get(campo)?.errors?.['pattern'])   return this.ErrorMsg[campo]['pattern']
-    if(this.registerForm.get(campo)?.errors?.['email'])     return this.ErrorMsg[campo]['email']
-    if(this.registerForm.get(campo)?.errors?.['notEqual'])  return this.ErrorMsg[campo]['notEqual']
-    if(this.registerForm.get(campo)?.errors?.['invalidNumber'])  return this.ErrorMsg[campo]['invalidNumber']
-    return ''
-  }
 }
